647425de87e178e8bfa96ea9b9248cfc49e381fc
Luis <xxcmgxx02@gmail.com>
2020-10-12 21:05:10 -0700

    Open Area Movement 1.0
    Added player movement
    - canMove boolean set to true
    - Created Prefab in Assets\Prefabs

---
+++ Turnout/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpd = 1f;
+
+    public Rigidbody2D rb;
+    public bool canMove;
+
+    Vector2 movement;
+
+    public void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        canMove = true;
+
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    //Frame-rate independent
+    private void FixedUpdate()
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+        if (canMove)
+        {
+            rb.MovePosition(rb.position + movement * moveSpd * Time.fixedDeltaTime);
+        }
+    }
+}
